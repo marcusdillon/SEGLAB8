7d971127753713c1436614da847c53004f56074e
Geshanth <91501039+Geshanth@users.noreply.github.com>
2023-03-19 16:17:51 -0400

    Add files via upload

---
+++ myapp/src/navAppointment.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class NavAppointment extends Component {
+    render () { 
+        return (
+            <h2>About</h2>
+        )
+    }
+}
+
+export default NavAppointment
