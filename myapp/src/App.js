ca792653d341ecbf80caf85c3ed66e3e23685ca8
Geshanth <91501039+Geshanth@users.noreply.github.com>
2023-03-19 16:20:02 -0400

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,7 +1,7 @@
 
     
 
-import React, {Component} from 'react';
+import React, {Component, useState } from 'react';
 import News from './News';
 import Banner from './Banner';
 import Footer from './Footer';
@@ -9,7 +9,11 @@ import Navbar from './Navbar';
 import InvestingResources from './InvestingResources';
 import Appointment from './Appointment';
 import Portoflio from './Portfolio';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import NavAppointment from './navAppointment';
+
 function App() {
+
   return (
     
     <div className="App">
@@ -17,30 +21,34 @@ function App() {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>  
-      <div className="wholeBody"> 
-        <div className='navHeader'>
-        <Banner/>
+      <Router>
+        <div className="wholeBody"> 
+          <div className='navHeader'>
+          <Banner/>
+          </div>
+          <div className='navNav'>
+            <Navbar/>
+          </div>
+          <div className='navNews'>
+            <News/>
+          </div>
+          <div className='navResources'>
+            <InvestingResources/>
+          </div>
+          <div className='navAppointment'>
+            <Appointment/>
+            <Routes>
+              <Route path='/navAppointment' element={<NavAppointment/>}/>
+            </Routes>
+          </div>
+          <div className='navPortfolio'>
+            <Portoflio/>
+          </div>
+          <div className='navFooter'>
+            <Footer/>
+          </div>
         </div>
-        <div className='navNav'>
-          <Navbar/>
-        </div>
-        <div className='navNews'>
-          <News/>
-        </div>
-        <div className='navResources'>
-          <InvestingResources/>
-        </div>
-        <div className='navAppointment'>
-          <Appointment/>
-        </div>
-        <div className='navPortfolio'>
-          <Portoflio/>
-        </div>
-        <div className='navFooter'>
-          <Footer/>
-        </div>
-      </div>
-      
+      </Router>
     </div>
   );
 }
