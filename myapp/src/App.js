a0400fc76be001dcd5f8200438d90bacd0eb5daa
Geshanth <91501039+Geshanth@users.noreply.github.com>
2023-03-19 20:46:41 -0400

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -22,7 +22,7 @@ function App() {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>  
       <Router>
-        <div className="wholeBody"> 
+        <div class="wholeBody"> 
           <div className='navHeader'>
           <Banner/>
           </div>
