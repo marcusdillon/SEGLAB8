f8f73dba47b0dc91c27d2288307954415be581c4
Geshanth <91501039+Geshanth@users.noreply.github.com>
2023-03-26 23:19:20 -0400

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -9,12 +9,14 @@ import Appointment from './Appointment';
 import Portoflio from './Portfolio';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import NavAppointment from './navAppointment';
+import LanguageSwitcher from './components/LanguageSwitcher';
 
 function App() {
 
   return (
     
     <div className="App">
+    <LanguageSwitcher />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
