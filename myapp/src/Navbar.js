e23e939101e3eff8c26eed5b0faaec12f85ff734
Geshanth <91501039+Geshanth@users.noreply.github.com>
2023-03-26 23:22:46 -0400

    Update Navbar.js

--- myapp/src/Navbar.js
+++ myapp/src/Navbar.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import './i18n';
+import Home from './components/Home';
+import FinNews from './components/FinNews';
+import InvResources from './components/InvResources';
+import Book from './components/Book';
+import Port from './components/Port';
+import Contact from './components/Contact';
 
 class Navbar extends Component {
     render () { 
@@ -11,12 +18,12 @@ class Navbar extends Component {
                     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
                     <div className='theNavBar'>
                         <ul>
-                            <li><Link to="/navNav">Home</Link></li>
-                            <li><Link to="/navNews">Financal News</Link></li>
-                            <li><Link to="/navResources">Investing Resources</Link></li>
-                            <li><Link to="/navAppointment">Book an Appointment</Link></li>
-                            <li><Link to="/navPortfolio">Portoflio</Link></li>
-                            <li><Link to="/navFooter">Contact</Link></li>
+                            <li><Link to="/navNav"><Home /></Link></li>
+                            <li><Link to="/navNews"><FinNews /></Link></li>
+                            <li><Link to="/navResources"><InvResources /></Link></li>
+                            <li><Link to="/navAppointment"><Book /></Link></li>
+                            <li><Link to="/navPortfolio"><Port /></Link></li>
+                            <li><Link to="/navFooter"><Contact /></Link></li>
                         </ul>
                     </div>
                 </div>
